12e4566c07c2cc7a307b33d1fb590870077d19d3
work <amitsaini95>
2022-10-31 18:55:09 +0530

    add jquery

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 $("document").ready(function () {
   var heading = [];
-
   $("#DemoButton1").on("click", function () {
     if (typeof (Storage) !== "undefined") {
       if (localStorage.getItem('heading') && localStorage.getItem('heading').length > 0)
@@ -8,61 +7,34 @@ $("document").ready(function () {
       var headings = $('.heading').val()
       heading.push(headings)
       localStorage.setItem('heading', JSON.stringify(heading));
-
     }
-
     $('main').append('<section><h1>' + headings + '</h1></section>');
-
     $('#form2 #select2').each(function (key) {
-
       $('#form2 #select2').append('<option value="' + key + '">' + headings + '</option>');
       key = key + 1;
-
-
-
     })
     $('#form3 #select3').each(function (key) {
       $(this).append('<option value="' + key + '">' + headings + '</option>');
       key = key + 1;
-
     })
-
-
-
-
-
   })
   var subheading = [];
   $("#DemoButton2").on("click", function () {
-
     if (typeof (Storage) !== "undefined") {
       if (localStorage.getItem('subheading') && localStorage.getItem('subheading').length > 0)
         subheading = JSON.parse(localStorage.getItem('subheading'));
       var subhead = $('.subheading').val()
       subheading.push(subhead)
       localStorage.setItem('subheading', JSON.stringify(subheading))
-
       $('main section ').append('<div><h3>' + subhead + '</h3></div>');
-
       $('#form3 #sub-select1').each(function (key) {
-
         $(this).append('<option value="' + key + '">' + subhead + '</option>');
         key = key + 1;
-
       })
     }
-
-
-
-
   })
-
-
   var hs = [];
-
   $('#DemoButton3').on("click", function () {
-
-
     var inputtypes = $('.inputtypes ').val();
     var inputlabel = $('.input-label').val();
     var inputclass = $('.input-class').val()
@@ -78,19 +50,7 @@ $("document").ready(function () {
       $('main ').append('<input type="' + inputtypes + '" class="' + inputclass + '" id="' + inputid + '"  name="' + inputname + '" value="' + inputvalue + '">')
       $('main').append('<label for="' + inputvalue + '">' + inputvalue + '</label>')
     }
-
-
-
-
-
-
-
-
-
-
   })
-
-
 })
 
 
